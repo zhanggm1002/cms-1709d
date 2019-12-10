71980e4bd31a98c5b28888ba7631fffbad655991
zhanggm <zhanggm1002@qq.com>
2019-12-10 15:50:26 +0800

    初始化项目，已测试mvc和dao

---
+++ zhangguoming-cms/src/main/java/com/zhangguoming/cms/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.zhangguoming.cms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+	@RequestMapping("")
+	public String index() {
+		return "index";
+	}
+}
---
+++ zhangguoming-cms/src/main/java/com/zhangguoming/cms/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.zhangguoming.cms.dao;
+
+import java.util.List;
+
+import com.zhangguoming.cms.pojo.User;
+
+public interface UserDao {
+	List<User> select();
+}
