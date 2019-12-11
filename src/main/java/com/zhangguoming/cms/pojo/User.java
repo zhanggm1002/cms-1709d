d704ccebac68815031efb2c9dcd1078a2ecf8920
zhanggm <zhanggm1002@qq.com>
2019-12-11 23:20:20 +0800

    修改user

--- src/main/java/com/zhangguoming/cms/pojo/User.java
+++ src/main/java/com/zhangguoming/cms/pojo/User.java
@@ -1,110 +1,138 @@
 package com.zhangguoming.cms.pojo;
 
 import java.io.Serializable;
-import java.sql.Date;
-/**
- * @Description:User类   
- * @author: zhanggm1002
- * @date:   2019年12月10日 下午3:26:24
- */
+import java.util.Date;
+
 public class User implements Serializable{
-	/*
-	 * 序列化版本号
-	 */
+    /**   
+	 * @Fields serialVersionUID : TODO(这个变量表示什么)   
+	 */  
 	private static final long serialVersionUID = 1L;
+
 	private Integer id;
-	private String username;
-	private String password;
-	private String nickname;
-	private Date birthday;
-	private Integer gender;
-	private Integer locked;
-	private Integer score;
-	private String role;
-	private String url;
-	private Date create_time;
-	private Date update_time;
-	
-	@Override
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    private Date birthday;
+
+    private Integer gender;
+
+    private Integer locked;
+
+    private String score;
+
+    private String role;
+
+    private String url;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    @Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", password=" + password + ", nickname=" + nickname
 				+ ", birthday=" + birthday + ", gender=" + gender + ", locked=" + locked + ", score=" + score
-				+ ", role=" + role + ", url=" + url + ", create_time=" + create_time + ", update_time=" + update_time
-				+ "]";
+				+ ", role=" + role + ", url=" + url + ", createTime=" + createTime + ", updateTime=" + updateTime + "]";
 	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getNickname() {
-		return nickname;
-	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-	public Date getBirthday() {
-		return birthday;
-	}
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-	public Integer getGender() {
-		return gender;
-	}
-	public void setGender(Integer gender) {
-		this.gender = gender;
-	}
-	public Integer getLocked() {
-		return locked;
-	}
-	public void setLocked(Integer locked) {
-		this.locked = locked;
-	}
-	public Integer getScore() {
-		return score;
-	}
-	public void setScore(Integer score) {
-		this.score = score;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public Date getCreate_time() {
-		return create_time;
-	}
-	public void setCreate_time(Date create_time) {
-		this.create_time = create_time;
-	}
-	public Date getUpdate_time() {
-		return update_time;
-	}
-	public void setUpdate_time(Date update_time) {
-		this.update_time = update_time;
-	}
-	
-	
 
+	public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Integer getLocked() {
+        return locked;
+    }
+
+    public void setLocked(Integer locked) {
+        this.locked = locked;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score == null ? null : score.trim();
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role == null ? null : role.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 }
